d6ac997c07192c9f8ff7ca23950c50c761e0bc7c
rovogit <tequi11az@mail.ru>
2020-10-26 12:45:31 +0500

    Задание 1,2 - правки

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
 <?php
-$is_auth = rand(0, 1);
+$is_auth = (bool)rand(0, 1);
 $user_avatar = '/img/user.jpg';
 $user_name = 'RoVo'; // укажите здесь ваше имя
 $categories = [
@@ -13,40 +13,40 @@ $categories = [
 
 $ads = [
     [
-        'Название' => '2014 Rossignol District Snowboard',
-        'Категория' => 'Доски и лыжи',
-        'Цена' => '10999',
-        'URL картинки' => 'img/lot-1.jpg',
+        'name' => '2014 Rossignol District Snowboard',
+        'category' => 'Доски и лыжи',
+        'cost' => '10999',
+        'image_url' => 'img/lot-1.jpg',
     ],
     [
-        'Название' => 'DC Ply Mens 2016/2017 Snowboard',
-        'Категория' => 'Доски и лыжи',
-        'Цена' => '159999',
-        'URL картинки' => 'img/lot-2.jpg',
+        'name' => 'DC Ply Mens 2016/2017 Snowboard',
+        'category' => 'Доски и лыжи',
+        'cost' => '159999',
+        'image_url' => 'img/lot-2.jpg',
     ],
     [
-        'Название' => 'Крепления Union Contact Pro 2015 года размер L/XL',
-        'Категория' => 'Крепления',
-        'Цена' => '8000',
-        'URL картинки' => 'img/lot-3.jpg',
+        'name' => 'Крепления Union Contact Pro 2015 года размер L/XL',
+        'category' => 'Крепления',
+        'cost' => '8000',
+        'image_url' => 'img/lot-3.jpg',
     ],
     [
-        'Название' => 'Ботинки для сноуборда DC Mutiny Charocal',
-        'Категория' => 'Ботинки',
-        'Цена' => '10999',
-        'URL картинки' => 'img/lot-4.jpg',
+        'name' => 'Ботинки для сноуборда DC Mutiny Charocal',
+        'category' => 'Ботинки',
+        'cost' => '10999',
+        'image_url' => 'img/lot-4.jpg',
     ],
     [
-        'Название' => 'Курта для сноуборда DC Mutiny Charocal',
-        'Категория' => 'Одежда',
-        'Цена' => '7500',
-        'URL картинки' => 'img/lot-5.jpg',
+        'name' => 'Курта для сноуборда DC Mutiny Charocal',
+        'category' => 'Одежда',
+        'cost' => '7500',
+        'image_url' => 'img/lot-5.jpg',
     ],
     [
-        'Название' => 'Маска Oakley Canopy',
-        'Категория' => 'Разное',
-        'Цена' => '5400',
-        'URL картинки' => 'img/lot-6.jpg',
+        'name' => 'Маска Oakley Canopy',
+        'category' => 'Разное',
+        'cost' => '5400',
+        'image_url' => 'img/lot-6.jpg',
     ],
 ];
 
@@ -119,15 +119,15 @@ function sumFormat($number) {
             <?php foreach ($ads as $ad) : ?>
                 <li class="lots__item lot">
                     <div class="lot__image">
-                        <img src="<?php echo $ad['URL картинки']; ?>" width="350" height="260" alt="">
+                        <img src="<?php echo $ad['image_url']; ?>" width="350" height="260" alt="">
                     </div>
                     <div class="lot__info">
-                        <span class="lot__category"><?php echo $ad['Категория']; ?></span>
-                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?php echo $ad['Название']; ?></a></h3>
+                        <span class="lot__category"><?php echo $ad['category']; ?></span>
+                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html"><?php echo $ad['name']; ?></a></h3>
                         <div class="lot__state">
                             <div class="lot__rate">
                                 <span class="lot__amount">Стартовая цена</span>
-                                <span class="lot__cost"><?php echo sumFormat($ad['Цена']); ?></span>
+                                <span class="lot__cost"><?php echo sumFormat($ad['cost']); ?></span>
                             </div>
                             <div class="lot__timer timer">
                                 12:23
